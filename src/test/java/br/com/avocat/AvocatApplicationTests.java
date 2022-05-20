54bf43c91f683181bb085137abed354ef2ef8a7a
Avocat-Dev <avocat.sp.contato@gmail.com>
2022-05-20 16:40:24 -0300

    commit init

---
+++ src/test/java/br/com/avocat/AvocatApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.avocat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvocatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
