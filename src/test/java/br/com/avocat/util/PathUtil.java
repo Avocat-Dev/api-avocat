506f7a47a4e4849375bad332939e330b95b154cc
Avocat-Dev <avocat.sp.contato@gmail.com>
2022-05-31 22:24:06 -0300

    test: Criação da classe utilitaria para centralizar os paths a serem utilizados pelas classes de testes.

---
+++ src/test/java/br/com/avocat/util/PathUtil.java
@@ -0,0 +1,10 @@
+package br.com.avocat.util;
+
+public class PathUtil {
+
+	public final static String LOCAL_HOST = "http://localhost:";
+	
+	public static final String PATH_AUTH_TOKEN = "/v1/auth/token";
+	
+	public final static String PATH_ESCRITORIO = "/v1/escritorios";	
+}
