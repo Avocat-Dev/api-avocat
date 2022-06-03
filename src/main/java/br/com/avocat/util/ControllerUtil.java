ee2c4bc0970e1b00fb453125f6d140065edc7795
Avocat-Dev <avocat.sp.contato@gmail.com>
2022-06-02 22:02:28 -0300

    refactor: Melhoria na classe Util.

--- src/main/java/br/com/avocat/util/ControllerUtil.java
+++ src/main/java/br/com/avocat/util/ControllerUtil.java
@@ -6,19 +6,14 @@ import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 
 public class ControllerUtil {
-
-	public static <T> ResponseEntity<T> resolveNotFound(Optional<T> result) {
-		
-		if(result.isPresent()) {				
-			return ResponseEntity.ok().body(result.get());		
-		} else {
-			return ResponseEntity.notFound().build();
-		}
+	
+	public static <T> ResponseEntity<T> resolveNotFound() {		
+		return ResponseEntity.notFound().build();		
 	}
 
-	public static <T> ResponseEntity<T> resolveBadRequest(Optional<T> result) {
+	public static <T> ResponseEntity<T> resolve(Optional<T> result) {
 		
-		if(result != null) {				
+		if(result.isPresent()) {				
 			return ResponseEntity.ok().body(result.get());		
 		} else {
 			return ResponseEntity.badRequest().build();
