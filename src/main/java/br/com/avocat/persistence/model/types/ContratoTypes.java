30acdb220bfe957047dfa62c90e69e5c77f6c8ce
Avocat-Dev <avocat.sp.contato@gmail.com>
2022-06-03 22:32:01 -0300

    feat: Criacao tipos enumerados para Contrato

---
+++ src/main/java/br/com/avocat/persistence/model/types/ContratoTypes.java
@@ -0,0 +1,22 @@
+package br.com.avocat.persistence.model.types;
+
+import lombok.Getter;
+
+@Getter
+public enum ContratoTypes {
+
+	HORAS(0, "Horas"),
+    CUSTO_FIXO(1, "Custo Fixo"),
+    ESCALONADO_HORA(2, "Escalonado por Hora"),
+    ESCALONADO_PROCESSO(3, "Escalonado por Processo"),
+    SEM_CUSTO(4, "Sem Custo"),
+	ADMINISTRATIVO(3, "Administrativo");
+
+    private final int value;
+    private final String descricao;
+
+    ModalidadeTypes(Integer value, String descricao) {
+        this.value = value;
+        this.descricao = descricao;
+    }
+}
