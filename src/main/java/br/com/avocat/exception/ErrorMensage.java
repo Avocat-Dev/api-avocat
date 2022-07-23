04893cee3a32da64c4f4a469c8918d6fa25b2cfc
Avocat-Dev <avocat.sp.contato@gmail.com>
2022-07-23 11:15:54 -0300

    refactor: Melhoria na mensagens de Erros adicionando list para erros em lote.

--- src/main/java/br/com/avocat/exception/ErrorMensage.java
+++ src/main/java/br/com/avocat/exception/ErrorMensage.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @NoArgsConstructor
@@ -21,5 +23,13 @@ public class ErrorMensage {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
 	private LocalDateTime data;
 
-	private String mensagem;
+	private String erro;
+	private List<String> erros = new ArrayList<>();
+
+	public static ErrorMensage newInstance() {
+		ErrorMensage errorMensage = new ErrorMensage();
+		errorMensage.setUuid(UUID.randomUUID());
+		errorMensage.setData(LocalDateTime.now());
+		return errorMensage;
+	}
 }
--- src/main/java/br/com/avocat/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/avocat/exception/GlobalExceptionHandler.java
@@ -1,8 +1,6 @@
 package br.com.avocat.exception;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
-
+import br.com.avocat.util.ConstantesUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
@@ -15,47 +13,40 @@ import org.springframework.web.context.request.WebRequest;
 public class GlobalExceptionHandler {
 
 	static final Logger LOGGER = LogManager.getLogger(GlobalExceptionHandler.class);
-	
+
 	@ExceptionHandler(AvocatException.class)
 	public ResponseEntity<ErrorMensage> avocatException(AvocatException ex) {
-		ErrorMensage error = new ErrorMensage();
-
-		error.setUuid(gerarUUID(ex));
-		error.setStatusCode(HttpStatus.BAD_REQUEST.value());
-		error.setData(LocalDateTime.now());
-		error.setMensagem(ex.getMessage());
-
-		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+		var errorMensage = ErrorMensage.newInstance();
+		errorMensage.setStatusCode(HttpStatus.BAD_REQUEST.value());
+		capturarErros(ex, errorMensage);
+		return new ResponseEntity<>(errorMensage, HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ErrorMensage> globalExceptionHandler(Exception ex, WebRequest request) {
-		ErrorMensage error = new ErrorMensage();
-
-		error.setUuid(gerarUUID(ex));
-		error.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-		error.setData(LocalDateTime.now());
-		error.setMensagem(ex.getMessage());
-
-		return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+		var errorMensage = ErrorMensage.newInstance();
+		errorMensage.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+		capturarErros(ex, errorMensage);
+		return new ResponseEntity<>(errorMensage, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 	@ExceptionHandler(RecursoNaoEncontradoException.class)
 	public ResponseEntity<ErrorMensage> globalExceptionHandler(RecursoNaoEncontradoException ex, WebRequest request) {
-		ErrorMensage error = new ErrorMensage();
-
-		error.setUuid(gerarUUID(ex));
-		error.setStatusCode(HttpStatus.NOT_FOUND.value());
-		error.setData(LocalDateTime.now());
-		error.setMensagem(ex.getMessage());
-
-		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+		var errorMensage = ErrorMensage.newInstance();
+		errorMensage.setStatusCode(HttpStatus.NOT_FOUND.value());
+		capturarErros(ex, errorMensage);
+		return new ResponseEntity<>(errorMensage, HttpStatus.NOT_FOUND);
 	}
 
-	private UUID gerarUUID(Exception ex) {
-		var uuid = UUID.randomUUID();
+	private void capturarErros(Exception ex, ErrorMensage errorMensage) {
 
-		LOGGER.warn(uuid, ex);
-		return uuid;
+		if(ex.getMessage().contains(ConstantesUtil.SEPARADOR_ERROS)) {
+			String[] msgs = ex.getMessage().split(ConstantesUtil.SEPARADOR_ERROS);
+			for(String m : msgs)
+				errorMensage.getErros().add(m);
+		} else {
+			errorMensage.setErro(ex.getMessage());
+		}
+		LOGGER.warn(errorMensage.getUuid(), ex);
 	}
 }
